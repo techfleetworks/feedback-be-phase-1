091e2ae57197037c053873a618840eb1ec03689b
cjwe <christajwest@gmail.com>
2022-07-19 18:12:53 +0200

    add test evaluation form and began cheese eval form

---
+++ src/api/test-evaluation-form/services/test-evaluation-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * test-evaluation-form service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::test-evaluation-form.test-evaluation-form');
