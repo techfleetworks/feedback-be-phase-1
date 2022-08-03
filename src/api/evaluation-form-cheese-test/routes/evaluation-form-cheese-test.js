1eece335afb0210cfd61a65b6f7b5aecd18aa0e4
cjwe <christajwest@gmail.com>
2022-08-03 17:15:08 +0200

    Add cheese test form

---
+++ src/api/evaluation-form-cheese-test/routes/evaluation-form-cheese-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * evaluation-form-cheese-test router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::evaluation-form-cheese-test.evaluation-form-cheese-test');
